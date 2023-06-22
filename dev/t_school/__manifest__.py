bf1fd66b3bad8e8e8666119465f70ff7a55ed932
Jargalsaikhan <bazooka.mgl@gmail.com>
2023-06-22 17:20:44 +0800

    a

--- dev/t_school/__manifest__.py
+++ dev/t_school/__manifest__.py
@@ -4,6 +4,7 @@
     'website': 'https://store.epicgames.com/en-US/p/thehunter-call-of-the-wild',
     'summary': 'Дадлагааараа хийлээ гэж',
     'data':[
+        'security/ir.model.access.csv',
         'views/menu.xml',
         'views/student.xml',
     ]
