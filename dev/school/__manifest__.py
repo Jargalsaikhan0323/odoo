0bd1998a7674b34e0cf208a26d1aca8cc1b8e131
Jargalsaikhan <bazooka.mgl@gmail.com>
2023-06-22 13:24:39 +0800

    a

---
+++ dev/school/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'School',
+    'author': 'sda',
+    'website': 'www.odoomates.tech',
+    'summary': 'devellpossasdadmentdw',
+    'data':[
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient.xml',
+    ]
+}
