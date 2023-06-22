62f0f3da8491699c1fc3bd405519ff7654b302bf
Jargalsaikhan <bazooka.mgl@gmail.com>
2023-06-22 15:29:06 +0800

    a

--- dev/t_school/__manifest__.py
+++ dev/t_school/__manifest__.py
@@ -3,5 +3,8 @@
     'author': 'Жаргалсайхан',
     'website': 'https://store.epicgames.com/en-US/p/thehunter-call-of-the-wild',
     'summary': 'Дадлагааараа хийлээ гэж',
-    
+    'data':[
+        'views/menu.xml',
+        'views/student.xml',
+    ]
 }
--- dev/t_school/models/student.py
+++ dev/t_school/models/student.py
@@ -2,5 +2,10 @@ from odoo import api, fields, models
 
 class SchoolStudent(models.Model):
     _name = "school.student"
+    _description = "Student regist"
 
     name = fields.Char(string='Нэр',required=True)
+    age = fields.Integer(string='Нас')
+    Eysh = fields.Boolean(string="ЭЕШ өгсөн эсэх")
+    notes = fields.Text(string="Тэмдэглэл")
+    gender = fields.Selection([('male','Эр'), ('female','Эм')], string="Хүйс")
