f7d8a2e132ea775f8358c9c1419c121111956683
Jargalsaikhan <bazooka.mgl@gmail.com>
2023-06-26 12:56:12 +0800

    a

---
+++ dev/t_school/models/teacher.py
@@ -0,0 +1,11 @@
+from odoo import api, fields, models
+
+class SchoolStudent(models.Model):
+    _name = "school.teacher"
+    _description = "Teacher regist"
+
+    name = fields.Char(string='Нэр',required=True)
+    age = fields.Integer(string='Нас')
+    child = fields.Boolean(string="Хүүхэдтэй эсэх")
+    notes = fields.Text(string="Тэмдэглэл")
+    gender = fields.Selection([('male','Эр'), ('female','Эм')], string="Хүйс")
